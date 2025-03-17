63c758f71470adba6b365b5dfe08832fe5788732
jeffreyxazer <jeffreyxazer@gmail.com>
2025-03-17 15:29:53 +0300

    Project complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
 });
+
+const form = document.getElementById("create-task-form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); 
+
+  
+  const taskInput = document.getElementById("new-task-description");
+  const taskDescription = taskInput.value;
+
+  
+  const taskItem = document.createElement("li");
+  taskItem.textContent = taskDescription;
+
+  
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  taskItem.appendChild(deleteButton);
+
+  deleteButton.addEventListener("click", function () {
+    taskItem.remove(); 
+  });
+
+  const taskList = document.getElementById("tasks");
+  taskList.appendChild(taskItem);
+
+  taskInput.value = "";
+});
+
+const taskInput = document.getElementById("new-task-description");
+const taskDescription = taskInput.value;
+
+const taskItem = document.createElement("li");
+taskItem.textContent = taskDescription;
+
+const taskList = document.getElementById("tasks");
+taskList.appendChild(taskItem);
+
+taskInput.value = "";
